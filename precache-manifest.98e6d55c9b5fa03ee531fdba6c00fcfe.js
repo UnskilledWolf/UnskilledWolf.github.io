0d950278e1ae6a123c6ba2f54305eb5fa9d49817
GeroX13 <geronimo.xl13@gmail.com>
2019-10-23 22:30:40 -0700

    Updated to React Page

---
+++ precache-manifest.98e6d55c9b5fa03ee531fdba6c00fcfe.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91041e5f869919baeaff501bd6ab6fd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59ca4e2c25475f6f6282",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "f5b649564323e55c343d",
+    "url": "/static/css/main.931003d6.chunk.css"
+  },
+  {
+    "revision": "59ca4e2c25475f6f6282",
+    "url": "/static/js/2.1755490c.chunk.js"
+  },
+  {
+    "revision": "f5b649564323e55c343d",
+    "url": "/static/js/main.985b26a4.chunk.js"
+  },
+  {
+    "revision": "d1119b76defe338463f2",
+    "url": "/static/js/runtime-main.bca423f5.js"
+  },
+  {
+    "revision": "c53c9503fc83faf166946b83665ddc99",
+    "url": "/static/media/ImagePlaceholder.c53c9503.png"
+  },
+  {
+    "revision": "7a63c3096aa76c6449b9703946bc2f1c",
+    "url": "/static/media/games.7a63c309.svg"
+  },
+  {
+    "revision": "05d4eb4cb7e7df0b34f7fdbf4cbda47f",
+    "url": "/static/media/index_art.05d4eb4c.png"
+  },
+  {
+    "revision": "5326b16994a6724e6f1a1c3381f6b3b4",
+    "url": "/static/media/index_code.5326b169.png"
+  },
+  {
+    "revision": "d1c1f2db8b4c8d0e77a09068aefa59ab",
+    "url": "/static/media/index_cover_paralax.d1c1f2db.png"
+  },
+  {
+    "revision": "a0d18f5a372b34e93424996f5d1f126e",
+    "url": "/static/media/index_games.a0d18f5a.png"
+  },
+  {
+    "revision": "1dc77e3577d8f362321aaed232f152cc",
+    "url": "/static/media/sites.1dc77e35.svg"
+  }
+]);
